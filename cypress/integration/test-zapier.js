29ac04f890dbd38f4af7129c591918b4c741d53f
ngaruko <ngarukob@yahoo.com>
2025-02-14 01:57:38 +0000

    click accept

--- cypress/integration/test-zapier.js
+++ cypress/integration/test-zapier.js
@@ -10,11 +10,22 @@ describe('Navigate to PR URL', () => {
         cy.visit(prUrl);
   
         // You can add more tests here to interact with the page
-        cy.title().should('include', 'Pre-Booking Offer');
-        cy.contains('button', 'Accept').click();
-        cy.contains('Booking Accepted').should('be.visible');
-        // cy.contains('Already Accepted')
+        //cy.title().should('include', 'Pre-Booking Offer');
+        // cy.contains('button', 'Accept').click();
+        // cy.contains('Booking Accepted').should('be.visible');
+        // Check for the 'Booking Accepted' message
+        cy.title().should('include', 'Pre-Booking Offer').then(() => {
+          cy.contains('button', 'Accept').click();
+          cy.contains('Booking Accepted').should('be.visible');
+          console.log('Booking has been successfully accepted.');
+        }).catch((err) => {
+          // Handle the error if 'Booking Accepted' is not found
+          console.error('Cannot confirm booking acceptance:', err.message);
+          cy.contains('h2.text-center', 'Already Accepted').should('be.visible');
 
+        });
+        // cy.contains('h2.text-center', 'Already Accepted').should('be.visible');
+        
       } else {
         cy.log('No URL provided');
       }
